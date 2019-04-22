275192be7d1e6e1a5074ade249abde39bf77a7ad
Julio Berina <julioberina@gmail.com>
2019-04-22 01:45:48 -0700

    Write menu data to .json file

--- scraper.rb
+++ scraper.rb
@@ -1,18 +1,41 @@
 require 'nokogiri'
 require 'httparty'
 require 'byebug'
+require 'json'
 
 def scraper
   url = 'https://postmates.com/merchant/ozen-sushi-buena-park'
   unparsed = HTTParty.get(url)
   parsed = Nokogiri::HTML(unparsed)
-  menu_items = parsed.css('div[id^="category-"]')
-  popular_item = menu_items.shift
-  puts popular_item.css('h2 > div > span').text
-
-  menu_items.each do |item|
-    puts item.css('h2').text
+  data = {}
+  menu_categories = parsed.css('div[id^="category-"]')
+  popular_items = menu_categories.shift
+  popular_items_data = []
+  popular_items.css('div > div.product-container').each do |item|
+    food = {}
+    food['name'] = item.css('h3.product-name').text || ''
+    food['description'] = item.css('div.product-description').text || ''
+    food['price'] = item.css('div > div > span').text || ''
+    popular_items_data << food
   end
+
+  data[popular_items.css('h2 > div > span').text] = popular_items_data
+
+  menu_categories.each do |category|
+    menu_items = []
+
+    category.css('div > div.product-container').each do |item|
+      food = {}
+      food['name'] = item.css('h3.product-name').text || ''
+      food['description'] = item.css('div.product-description').text || ''
+      food['price'] = item.css('div > div > span').text || ''
+      menu_items << food
+    end
+
+    data[category.css('h2').text] = menu_items
+  end
+
+  File.open("menu.json", "w") { |file| file.write data.to_json }
 end
 
 scraper
